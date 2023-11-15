62c0e58b7494dc07c488536e7cd07cd592108669
IntelMin <intelmin0915@gmail.com>
2023-11-15 18:37:01 +0530

    exclusive pages added

--- app/(comingsoon)/liveMonitoring/page.tsx
+++ app/(comingsoon)/liveMonitoring/page.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import ComingSoon from "@/components/comingsoon";
+import { NavbarWrapper } from "@/components/navbar/navbar";
+import Title from "@/components/title";
+import SecurityLeaderBoard from "@/components/security/SecurityLeaderBoard";
+import LiveMetric from "@/components/liveMonitoring/LiveMetric";
 
 type Props = {};
 
 const LiveMonitoring = (props: Props) => {
   return (
-    <div className="py-4 px-5 w-full h-[100vh] flex flex-col gap-4 bg-[#2121219f]">
-      <ComingSoon />
-    </div>
+    <NavbarWrapper pageTitle={<div></div>}>
+      <div className="py-4 px-5 w-full h-full flex flex-col gap-4">
+        <Title title="Live Monitoring" />
+        <LiveMetric />
+        <SecurityLeaderBoard search />
+      </div>
+    </NavbarWrapper>
   );
 };
 
---
+++ components/bugBounty/bountyCard.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+    name?: string;
+    imgUrl?:string;
+    security?:string;
+    maxReward?:string;
+    lastUpdated?:string;
+    assests?:string;
+};
+
+const BountyCard = (props: Props) => {
+  return (
+    <div className="p-3 bg-[#80808058] rounded-lg col-span-1">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={`${props?.imgUrl}`}
+            alt="webmix"
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+          <h2 className="font-[600] uppercase text-[20px]">{props?.name}</h2>
+        </div>
+        <div>
+          <h1 className="bg-[#0080005a] px-2 py-[5px] rounded-md w-fit font-bold">
+            {props?.security}
+          </h1>
+        </div>
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="mt-2">
+          <h1 className="text-[12px]">Max Reward</h1>
+          <h1 className="text-success text-[24px] font-semibold">{props?.maxReward}</h1>
+          <h1 className="text-[12px]">
+            Last Updated <span className="font-semibold">{props?.lastUpdated}</span>
+          </h1>
+        </div>
+        <div className="flex gap-2 items-center bg-[#779d7755] p-2 rounded-md">
+          <h1 className="text-[14px]">Assets</h1>
+          <p className="text-[16px] font-semibold">{props?.assests}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BountyCard;
