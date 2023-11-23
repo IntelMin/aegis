edb933fef1be4a62465a5c45421bd2b8c3d24b86
IntelMin <intelmin0915@gmail.com>
2023-11-24 00:45:41 +0100

    Fix formatting

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 9898;
 
-const infoRoute = require('./api/info');
-const describeRoute = require('./api/describe');
-const markdownRoute = require('./api/markdown');
-const dependencyRoute = require('./api/dependencies');
-const codeRoute = require('./api/code');
+const infoRoute = require("./api/info");
+const describeRoute = require("./api/describe");
+const markdownRoute = require("./api/markdown");
+const dependencyRoute = require("./api/dependencies");
+const codeRoute = require("./api/code");
 
-const trendingTokens = require('./api/trending');
+const trendingTokens = require("./api/trending");
+const deployer = require("./api/deployer");
 // const graphRoute = require('./routes/graph');
 
-app.use('/info', infoRoute);
-app.use('/describe', describeRoute);
-app.use('/markdown', markdownRoute);
-app.use('/dependency', dependencyRoute);
-app.use('/code', codeRoute);
+app.use(express.json());
 
-app.use('/trending', trendingTokens);
+app.use("/info", infoRoute);
+app.use("/describe", describeRoute);
+app.use("/markdown", markdownRoute);
+app.use("/dependency", dependencyRoute);
+app.use("/code", codeRoute);
+
+app.use("/trending", trendingTokens);
+app.use("/deployer", deployer);
 // app.use('/graph', graphRoute);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
