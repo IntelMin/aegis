d064977d7adf126046521c2b8fa865041b30cf3e
IntelMin <intelmin0915@gmail.com>
2023-11-24 02:36:47 +0530

    integrate update-code and code api into UI

--- components/deployer/index.tsx
+++ components/deployer/index.tsx
@@ -1,20 +1,45 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import { Input, Button } from "@nextui-org/react";
 import { BsFillSendFill } from "react-icons/bs";
-
+import axios from "axios";
 const editorOptions = {
   minimap: {
     enabled: true,
   },
+  readonly:true,
   fontSize: 14,
   cursorStyle: "block",
   wordWrap: "on",
 }
 
 const Deployer = () => {
+  const [code, setCode] = useState('');
+  const [status, setStatus] = useState({});
+  const [prompt, setPrompt] = useState('');
+
+  useEffect(() => {
+    axios.get('/deployer/code')
+      .then(response => {
+        setCode(response.data.code);
+        setStatus(response.data.status);
+      });
+  }, []);
+  const handleSend = () => {
+    axios.post('/deployer/update-code', { prompt })
+      .then(response => {
+        setCode(response.data.code);
+        setStatus(response.data.status);
+      });
+  };
+
+  const handleKeyPress = (event:any) => {
+    if (event.key === 'Enter') {
+      handleSend();
+    }
+  };
   return (
     <div className="grid grid-rows-1 grid-cols-[1fr,auto] gap-4 h-full">
       <div className="grid grid-rows-[1fr,auto] gap-4">
@@ -24,14 +49,18 @@ const Deployer = () => {
           language="solidity"
           defaultLanguage="javascript"
           defaultValue=""
+          code = {code}
           options={editorOptions}
         />
         <div className="grid grid-rows-[1fr,auto] h-64 p-2 gap-2 rounded-md bg-zinc-700">
           <div></div>
           <Input
             size="sm"
+            value={prompt}
+            onChange={e => setPrompt(e.target.value)}
+            onKeyDown={handleKeyPress}
             endContent={
-              <Button color="primary" variant="light" size="sm" isIconOnly>
+              <Button color="primary" variant="light" size="sm" isIconOnly onClick={handleSend}>
                 <BsFillSendFill />
               </Button>  
             }
