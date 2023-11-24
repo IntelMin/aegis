44e862403411b40f93c47d108646fe4ceccb84c1
IntelMin <intelmin0915@gmail.com>
2023-11-24 17:19:50 +0530

    refactor: adding null check in stats

--- components/Dashboard/DashboarStastCard.tsx
+++ components/Dashboard/DashboarStastCard.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode } from 'react';
-import { Card } from '@nextui-org/react';
+import React, { ReactNode } from "react";
+import { Card } from "@nextui-org/react";
 
 interface DashboardStatsCardProps {
   data: {
@@ -8,13 +8,16 @@ interface DashboardStatsCardProps {
   children?: ReactNode;
 }
 
-const DashboardStatsCard: React.FC<DashboardStatsCardProps> = ({ data, children }: DashboardStatsCardProps) => {
+const DashboardStatsCard: React.FC<DashboardStatsCardProps> = ({
+  data,
+  children,
+}: DashboardStatsCardProps) => {
   const [key, value] = Object.entries(data)[0];
 
   return (
-    <Card className='w-[15%] p-4 mr-2 bg-opacity-50 w-full md:w-[80%] bg-gradient-to-tl from-[#1b1b1bbd] via-[#1b1b1bbd] to-gray-700'>
-      <div className='text-center text-2xl font-bold'>{value}</div>
-      <div className='text-center'>{key}</div>
+    <Card className="w-[15%] p-4 mr-2 bg-opacity-50 w-full md:w-[80%] bg-gradient-to-tl from-[#1b1b1bbd] via-[#1b1b1bbd] to-gray-700">
+      <div className="text-center text-2xl font-bold">{value ? value : "--"}</div>
+      <div className="text-center">{key}</div>
       {children}
     </Card>
   );
