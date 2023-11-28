fdf509ea91091b0fef7de083c2c288926ab70c51
IntelMin <intelmin0915@gmail.com>
2023-11-29 01:41:59 +0530

    fix

--- server/api/audit.js
+++ server/api/audit.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const { isERC20Token, isContractOpenSource } = require("../utils");
+const { isERC20Token, isContractOpenSource, readCache } = require("../utils");
 const router = express.Router();
 
 router.post("/:address", async (req, res) => {
@@ -53,15 +53,76 @@ router.get("/:address", async (req, res) => {
     const {data: auditRequests, error} = await supabase.from('audit-requests').select('*').eq('address', address);
     if(auditRequests[0].status ==="pending"){
         return res.status(200).send("Contract is in queue, please wait for the audit to finish");
-    }
-    else if(auditRequests[0].status ==="partial"){
-        return res.status(200).send("Contract is Partially complete , please wait for complete audit to finish");
-    } else if(auditRequests[0].status ==="complete"){
+    }else{
 
-        return res.status(200).send("Contract is complete");
+    let filedata = readCache(`./contracts/${address}.json`);
+    let source_code = filedata["source_code"];
+    let filename = `./data/${address}/`;
+    const securityCacheFile = path.join(__dirname, `../data/${address}/security.json`);
+    const rugpullCacheFile = path.join(__dirname, `../data/${address}/rugpull.json`);
+    const tokenCacheFile = path.join(__dirname, `../data/${address}/token.json`);
+    const metadataCacheFile = path.join(__dirname, `../data/${address}/meta.json`);
+    const treeCacheFile = path.join(__dirname, `../data/${address}/tree.json`);
+    const statsCacheFile = path.join(__dirname, `../data/${address}/stats.json`);
+    const findingsCacheFile = path.join(__dirname, `../data/${address}/findings.json`);
+    const securityCache = await readCache(securityCacheFile);
+    const rugpullCache = await readCache(rugpullCacheFile);
+    const tokenCache = await readCache(tokenCacheFile);
+    const metadataCache = await readCache(metadataCacheFile);
+    const treeCache = await readCache(treeCacheFile);
+    const statsCache = await readCache(statsCacheFile);
+    const findingsCache = await readCache(findingsCacheFile);
+
+    const solidity = filedata;
+    delete solidity["abi"];
+    delete solidity["creation_bytecode"];
+    delete solidity["source_code"];
+    delete solidity["deployed_bytecode"];
+    delete solidity["decoded_constructor_args"];
+    delete solidity["sourcify_repo_url"];
+    
+
+    const files = []
+    for (let i = 0; i < solidity["additional_sources"].length; i++) {
+      files.push(solidity["additional_sources"][i]["file_path"])
+    }
+
+    delete solidity["additional_sources"];
+
+
+     if(auditRequests[0].status ==="partial"){
+        // return res.status(200).send("Contract is Partially complete , please wait for complete audit to finish");
+        return   res.status(200).send({
+            tree: treeCache,
+            code: source_code,
+            findings: "pending",
+            files: files,
+            solidity: solidity,
+            metadata:metadataCache,
+            security:securityCache,
+            rugpull:rugpullCache,
+            token:tokenCache,
+            stats:statsCache
+
+          });
+        } else if(auditRequests[0].status ==="complete"){
+            
+            // return res.status(200).send("Contract is complete");
+            return   res.status(200).send({
+                tree: treeCache,
+                code: source_code,
+                findings: findingsCache,
+                files: files,
+                solidity: solidity,
+                metadata:metadataCache,
+                security:securityCache,
+                rugpull:rugpullCache,
+                token:tokenCache,
+                stats:statsCache
+              });
     }
     else{
         return res.status(200).send("Contract is not in queue");
-    }
+    }}
 });
 module.exports = router;
