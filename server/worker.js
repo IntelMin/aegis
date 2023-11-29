68d242bc2e78602fef3d74f012841439b279bb1a
IntelMin <intelmin0915@gmail.com>
2023-11-29 10:37:46 -0700

    Revert "add delay between openai request"
    This reverts commit 99c157c603cce10af91553bb98fc688ba53d0d95.

--- server/worker.js
+++ server/worker.js
@@ -187,10 +187,6 @@ async function getFindings(codeSegments) {
       // if (i > 0) {
       //   break;
       // }
-
-      if (i < codeSegments.length - 1) {
-        await new Promise(resolve => setTimeout(resolve, 5000))
-      }
     }
   
     return findings;
@@ -316,8 +312,11 @@ async function definedRequest(address) {
   //GPT code audit part 
 async function worker() {
   const { data: auditRequests, error:error_req } = await supabase
-  .from('audit-requests')
-  .select('*')
+    .from('audit-requests')
+    .select('*')
+    .eq('status', 'pending')
+    .or('status', 'partial');
+  
 
   //sorting pending and partial audits
   auditRequests.sort((a, b) => {
@@ -330,7 +329,7 @@ async function worker() {
     }
   });
   console.log("auditRequests: ", auditRequests);
-  auditRequests.map(async (row) => {
+  auditRequests.forEach(async (row) => {
     const address = row.address;
     if(row.status === "pending"){
       try {
@@ -351,11 +350,7 @@ async function worker() {
           address
         );
         const metadata = await getMetadata(address);
-        const keys = Object.keys(token_security.result);
-        const parse_security = token_security.result[keys[0]];
-        let parse_rugpull = token_rugpull["result"];
-        let parse_meta = metadata["tokens"][0];
-
+  
         //save token contract 
 
         let filename = `./contracts/${address}.json`;
