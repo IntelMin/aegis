e93b13caad07ac11a2f2975848d1be13e60aa242
IntelMin <intelmin0915@gmail.com>
2023-12-17 15:44:25 +0530

    next-auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,28 +3,26 @@ import { Layout } from "@/components/layout";
 import { fontSans } from "@/config/fonts";
 import clsx from "clsx";
 import "@/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import "@rainbow-me/rainbowkit/styles.css";
 import NextTopLoader from "nextjs-toploader";
 import { Providers } from "../providers";
 
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className={clsx(
         "min-h-screen bg-black font-sans antialiased",
         fontSans.variable
       )}>
-        <WhitelistWrapper>
           <NextTopLoader color="#b0b0b0" />
           <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        {/* <WhitelistWrapper> */}
             <Layout>{children}</Layout>
+        {/* </WhitelistWrapper> */}
           </Providers>
-        </WhitelistWrapper>
       </body>
     </html>
-  </ClerkProvider>
 );
 
 export default RootLayout;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,46 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import supabase from "@/server/supabase"
+export const authOptions:AuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
 
-import NextAuth from "next-auth";
+CredentialsProvider({
+    name: 'Credentials',
+    credentials: {},
+    async authorize(credentials, req) {
+      // Add logic here to look up the user from the credentials supplied
+      const { email, password } = credentials
+      const { data:user, error } = await supabase.from('users').select().eq('email', email).single()
+        console.log(user)
+    //   const user = { id: "1", name: "J Smith", email: "" }
+        if (user) {
+            // Any object returned will be saved in `user` property of the JWT
+            return user
+        } else {
+            // If you return null or false then the credentials will be rejected
+            return null
+            // You can also Reject this callback with an Error or with a URL:
+            // throw new Error('error message') // Redirect to error page
+            // throw '/path/to/redirect'        // Redirect to a URL
+        }
+    }
+})
+    // ...add more providers here
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+  pages: {
+    signIn: '/signin',
+    signOut: '/logout',
+    error: '/login', // Error code passed in query string as ?error=
+    verifyRequest: '/login', // (used for check email message)
+  },
+  
+  
+}
 
-import { authOptions } from "./auth";
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions)
+export {handler as GET , handler as POST}
