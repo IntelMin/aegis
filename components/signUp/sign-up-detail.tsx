aa6fae259f9b5da29f9eee15a0f1053200e9e6d4
IntelMin <intelmin0915@gmail.com>
2023-12-19 15:26:08 +0530

    feat: added upload function

--- components/signUp/sign-up-detail.tsx
+++ components/signUp/sign-up-detail.tsx
@@ -38,6 +38,35 @@ const SignUpDetailForm = ({ signInData, setSignInData }: Props) => {
       };
     }
   };
+  const uploadImage = async () => {
+    if (!newImage) {
+      console.error('No image selected');
+      return;
+    }
+    if (!process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || !process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME) {
+      console.error('No API key provided');
+      return;
+    }
+  
+    const imageData = new FormData();
+    imageData.append('file', newImage);
+    imageData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
+    imageData.append('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
+    imageData.append('folder', 'Cloudinary-React'); // Secret key
+
+    try {
+      const uploadResponse = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
+        method: 'POST',
+        body: imageData,
+      });
+      const uploadedImageData = await uploadResponse.json();
+      const imageUrl = uploadedImageData.secure_url;
+      setPreview(null);
+      setNewImage(null);
+    } catch (error) {
+      console.log(error, 'Error while image upload');
+    }
+  };
   return (
     <div className="flex flex-col gap-2">
       <div className="w-[395px] grid grid-cols-2 gap-4">
