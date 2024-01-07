97150e9d6449a9181d7bd97163a00fb8a0dccbf9
IntelMin <intelmin0915@gmail.com>
2024-01-08 00:43:06 +0500

    fix middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,30 +1,30 @@
 import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 const middleware = withAuth(
+  function middleware(request) {
+    const token = request.nextauth?.token;
+    const pathname = request.nextUrl?.pathname;
+    console.log({ token, pathname });
 
-    function middleware(request) {
-        const token = request.nextauth?.token
-        const pathname = request.nextUrl?.pathname
-        console.log({ token, pathname })
-        if (pathname === "/sign-up") {
-            return NextResponse.next()
-        }
-        if (pathname === "/api/user") {
-            return NextResponse.next()
-        }
-        if (!token) {
-            const signinurl = new URL("/login", request.nextUrl?.origin)
-            return NextResponse.redirect(signinurl.toString())
-        }
-        if (pathname === "/pending") {
-            return NextResponse.next()
-        }
-        if (!token.whitelisted) {
-            const pending = new URL("/pending", request.nextUrl?.origin)
-            return NextResponse.redirect(pending.toString())
-        }
+    if (token) {
+      if (!token.whitelisted) {
+        const pending = new URL('/pending', request.nextUrl?.origin);
+        return NextResponse.redirect(pending.toString());
+      } else {
+        return NextResponse.next();
+      }
+    } else {
+      const allowedPaths = ['/login', '/sign-up', '/api/user', '/api/auth/*'];
 
-    },
+      if (allowedPaths.includes(pathname)) {
+        return NextResponse.next();
+      } else {
+        const signinurl = new URL('/login', request.nextUrl?.origin);
+        return NextResponse.redirect(signinurl.toString());
+      }
+    }
+  },
+  {
     callbacks: {
       authorized: ({ token }) => true,
     },
