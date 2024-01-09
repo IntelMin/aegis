d548417b3aa275763646556098e8f2d57dbd6a8e
IntelMin <intelmin0915@gmail.com>
2024-01-10 00:43:05 +0500

    revert main layout structure

--- frontend/src/app/(features)/layout.tsx
+++ frontend/src/app/(features)/layout.tsx
@@ -1,9 +1,33 @@
-import Layout from '@/components/layout';
+'use client';
 
-export default function UserLayout({
-  children,
-}: {
+import React from 'react';
+import Navbar from '@/components/navbar';
+import Sidebar from '@/components/sidebar/sidebar';
+import MobileSidebar from '@/components/sidebar/sidebar-mobile';
+
+type Props = {
   children: React.ReactNode;
-}) {
-  return <Layout>{children}</Layout>;
-}
+};
+
+const UserLayout = ({ children }: Props) => {
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <>
+      <div className="max-md:hidden">
+        <Sidebar />
+      </div>
+      {show && (
+        <div className="h-full min-h-[100vh] z-[9] pt-20 md:hidden bg-black fixed">
+          <MobileSidebar setShow={setShow} />
+        </div>
+      )}
+      <div className="relative w-full">
+        <Navbar show={show} setShow={setShow} />
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default UserLayout;
