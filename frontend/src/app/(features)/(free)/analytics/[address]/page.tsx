6fb779131d338d3600b66fb500aaa603e4714843
IntelMin <intelmin0915@gmail.com>
2024-01-16 06:45:45 +0500

    add new analytics base

--- frontend/src/app/(features)/(free)/analytics/[address]/page.tsx
+++ frontend/src/app/(features)/(free)/analytics/[address]/page.tsx
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useToast } from '@/components/ui/use-toast';
 import useTokenInfo from '@/hooks/useTokenInfo';
+import useLiveData from '@/hooks/useLiveData';
 
 import TokenHeader from '@/components/analytics/header';
+import TokenOverview from '@/components/analytics/token-overview';
 
 type Props = {
   params: {
@@ -15,23 +17,27 @@ type Props = {
 const Analytics = ({ params }: Props) => {
   const { toast } = useToast();
   const contractAddress = params.address;
-  const [liveData, setLiveData] = useState<any>(null);
   const [tokenDetails, setTokenDetails] = useState<any>(null);
-  const { isFetching, tokenMetaData, error } = useTokenInfo(
+  const { isFetching, tokenInfo, error } = useTokenInfo(
     contractAddress,
+    'meta',
     true
   );
+  const liveData = useLiveData(contractAddress);
 
   return (
-    <div className="flex flex-col px-4 md:px-10 mt-4 gap-9">
-      <TokenHeader
-        showTitle={true}
-        liveData={liveData}
-        metadata={tokenMetaData}
-      />
+    <div className="flex flex-col px-4 md:px-10 mt-4 gap-6">
+      <TokenHeader showTitle={true} liveData={liveData} metadata={tokenInfo} />
 
       <div className="grid grid-cols-4 gap-6 pb-3">
-        <div>{/* Token Detailed Info */}</div>
+        <div>
+          {/* Token Detailed Info */}
+          <TokenOverview
+            tokenMetaData={tokenInfo}
+            tokenDetails={tokenDetails}
+            liveData={liveData}
+          />
+        </div>
         <div className="col-span-4 md:col-span-3"></div>
       </div>
     </div>
