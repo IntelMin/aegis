ecf3602e3689a66daca1d0b7b2e8be7a93be9d86
IntelMin <intelmin0915@gmail.com>
2024-01-17 22:41:03 +0530

    feat: add report UI

---
+++ frontend/src/app/(features)/(paid)/audit/reports/dummydata.ts
@@ -0,0 +1,24 @@
+//! Delete this file after adding data from backend
+
+export const tableData = [
+    {
+        tokenIcon: '/icons/token/binance.svg',
+        tokenName: 'XYZ',
+        tokenInfo: 'XYZ Token',
+        tokenAddress: '0xabc123def456ghi789jkl012mno345pqr678stu',
+        auditDate: '14-02-24',
+        auditTime: '10:45 PM',
+        tokenAmount: 178,
+        percentageData: [30, 45, 25],
+    },
+    {
+        tokenIcon: '/icons/token/binance.svg',
+        tokenName: 'ABC',
+        tokenInfo: 'ABC Token',
+        tokenAddress: '0x1234567890abcdef1234567890abcdef12345678',
+        auditDate: '15-03-25',
+        auditTime: '08:15 AM',
+        tokenAmount: 245,
+        percentageData: [15, 60, 25],
+    },
+];
