65f8599e0bbb0cd75d29ed4e54bb2d8df11c538e
IntelMin <intelmin0915@gmail.com>
2024-02-12 08:22:18 +0530

    header for analytics has old and new index files now

--- frontend/src/components/analytics/header/index.tsx
+++ frontend/src/components/analytics/header/index.tsx
@@ -3,14 +3,12 @@ import Image from 'next/image';
 import copy from 'copy-to-clipboard';
 import { formatNumber } from '@/utils/format-number';
 import { formatAge } from '@/utils/format-age';
+import { formatAddress } from '@/utils/format-address';
 import { Skeleton } from '@/components/ui/skeleton';
 import { BiBadgeCheck, BiChevronDown, BiChevronUp } from 'react-icons/bi';
 import TokenValueContainer from './token-value-container';
 import { toast } from 'sonner';
 import { TokenPrice } from './token-price';
-import PaymentDialog from '@/components/payment/dialog';
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
-import { useRouter } from 'next/navigation';
 
 type Props = {
   showTitle: boolean;
@@ -20,8 +18,6 @@ type Props = {
 
 const TokenHeader = ({ showTitle, metadata, liveData }: Props) => {
   const [loading, setLoading] = React.useState(false);
-  const [submitting, setSubmitting] = React.useState<boolean>(false);
-  const router = useRouter();
 
   const handleCopy = (text: string) => () => {
     copy(text);
@@ -29,7 +25,7 @@ const TokenHeader = ({ showTitle, metadata, liveData }: Props) => {
   };
 
   return (
-    <div className="">
+    <div className="container p-0 mx-auto">
       <div className="flex flex-wrap max-md:flex-col md:items-center justify-around gap-4">
         <div
           className={`${
@@ -80,6 +76,58 @@ const TokenHeader = ({ showTitle, metadata, liveData }: Props) => {
           <TokenPrice liveData={liveData} />
         </div>
 
+        <div className="flex max-md:hidden flex-end">
+          <div className="flex flex-row items-center">
+            {metadata && liveData ? (
+              <>
+                <div className="flex flex-col mr-1">
+                  <p className="text-zinc-500 text-[14px] w-[70px] text-right">
+                    {metadata?.symbol}:
+                  </p>
+                  <p className="text-zinc-500 text-[14px] w-[70px] text-right">
+                    PAIR:{' '}
+                  </p>
+                </div>
+                <div className="flex flex-col">
+                  {/* TODO: "Ox" the x doesn't appear in the same way for both addresses */}
+                  <div
+                    className="flex text-blue-400 cursor-pointer font-mono text-sm"
+                    onClick={handleCopy(metadata.address)}
+                  >
+                    0x
+                    {formatAddress(metadata.address).substring(2).toUpperCase()}
+                    <Image
+                      src="/icons/copy.svg"
+                      alt="copy"
+                      width={16}
+                      height={16}
+                      className="ml-1"
+                    />
+                  </div>
+                  <div
+                    className="flex text-blue-400 cursor-pointer font-mono text-sm"
+                    onClick={handleCopy(liveData?.pairAddress)}
+                  >
+                    0x
+                    {formatAddress(liveData?.pairAddress)
+                      .substring(2)
+                      .toUpperCase()}
+                    <Image
+                      src="/icons/copy.svg"
+                      alt="copy"
+                      width={16}
+                      height={16}
+                      className="ml-1"
+                    />
+                  </div>
+                </div>
+              </>
+            ) : (
+              <Skeleton className="w-24 h-6" />
+            )}
+          </div>
+        </div>
+
         <div className="flex flex-row max-md:flex-wrap flex-1 gap-4 md:gap-2">
           {/* <button
             className={` m-0 md:ml-5 flex items-center justify-center text-center ${
@@ -108,48 +156,6 @@ const TokenHeader = ({ showTitle, metadata, liveData }: Props) => {
                 name="AGE"
                 value={formatAge(liveData.pairCreatedAt, true)}
               />
-              <div className="max-md:w-full flex flex-col items-center justify-center w-fit gap-3">
-                <div
-                  className={`bg-[#0E76FD] border-[#0E76FD] text-zinc-50 text-[16px] border font-[400] w-full px-2 py-6 flex items-center justify-center text-center transition-all ease-in duration-200`}
-                >
-                  <PaymentDialog
-                    service="quick"
-                    address={'0x55A8f6c6b3Aa58ad6D1f26f6AFeDEd78F32E19f4'}
-                    onPrep={async () => {
-                      return true;
-                    }}
-                    UnlockedElement={
-                      <div className="flex flex-row w-full items-center justify-center">
-                        {submitting ? (
-                          <>
-                            <AiOutlineLoading3Quarters className="animate-spin mr-2" />
-                            <p className="text-[16px] font-[600] text-zinc-50">
-                              Loading...
-                            </p>
-                          </>
-                        ) : (
-                          <>
-                            <p className="text-[16px] font-[600] text-zinc-50">
-                              Quick Audit
-                            </p>
-                          </>
-                        )}
-                      </div>
-                    }
-                    LockedElement={
-                      <div className="flex flex-row items-center hover:cursor-pointer">
-                        <p className="text-[16px] font-[600] text-zinc-50">
-                          Quick Audit
-                        </p>
-                      </div>
-                    }
-                    onSuccess={() => {
-                      setSubmitting(true);
-                      router.push(`/audit/token/${'0x55A8f6c6b3Aa58ad6D1f26f6AFeDEd78F32E19f4'}/quick`);
-                    }}
-                  />
-                </div>
-              </div>
             </>
           ) : (
             <>
